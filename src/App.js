0a0b20b2d25ea75bae9403fdd65a6661bfae4a8b
unknown <ritikaagarwal111222@gmail.com>
2024-03-24 00:12:45 +0530

    added predict page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import './App.css';
-import Home from './Components/Home';
-import About from './Components/About';
-import Work from './Components/Work';
-import Testimonials from './Components/Testimonials';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
+import "./App.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonials from "./Components/Testimonials";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Predict from "./Components/Predict";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <About/>
-      <Work/>
-      <Testimonials/>
-      <Contact/>
-      <Footer/>
+      <BrowserRouter>
+        <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Home />
+                  <Work />
+                  <About />
+                  
+                  {/* <Testimonials />
+                  <Contact /> */}
+                  <Footer />
+                </>
+              }
+            />
+            <Route path="predict" element={<Predict />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
